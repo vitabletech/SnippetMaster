81f61a0b71920e8f1579e01dfa3e5ce3ac2b9b7b
Mayank Singh Kushwah <mayank.kushwah@eagleview.com>
2023-04-25 18:12:16 +0530

    enhancement: validate files by pre-commit, validate json files etc

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        [
+          'docs',
+          'fix',
+          'refactor',
+          'revert',
+          'style',
+          'test',
+          'enhancement',
+          'feature',
+        ],
+      ],
+    },
+  };
